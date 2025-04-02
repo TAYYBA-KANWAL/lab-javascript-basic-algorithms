1cf9a495c7e2271a9e41853fddc300fec3e1ad0c
Tayyba Kanwal <taybarashid215@gmail.com>
2025-04-02 20:16:11 +0200

    W2-D3-javascript

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // Iteration 1: Names and Input
-
-
+const hacker1 = "Pedro ";
+console.log("The driver name is :" , hacker1);
+const hacker2 = "jose ";
+console.log("The navigators name is :" , hacker2);
 // Iteration 2: Conditionals
-
+if(hacker1.length >= hacker2.length ){
+    console.log( `${hacker1}`);
+}else{
+    console.log(`${hacker2}`);
+}
 
 // Iteration 3: Loops
+console.log(hacker1.toUpperCase());
+let reverseName = hacker1.reverse();
+
+console.log(reverseName);
